8e3d1143886b2b0294b8af7097bf388427b837a5
DOCENTE-A <DOCENTE-A@UCSP.AULAB>
2017-06-08 20:01:32 -0500

    first commit

---
+++ app/src/main/java/com/example/docente_a/webjson/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.docente_a.webjson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatTextView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        new ReadJSONFeedTask().execute("http://extjs.org.cn/extjs/examples/grid/survey.html");
+    }
+
+    public String readJSON(String address) {
+        URL url = null;
+        try {
+            url = new URL(address);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        };
+        StringBuilder stringBuilder = new StringBuilder();
+        HttpURLConnection urlConnection = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            InputStream content = new BufferedInputStream(
+                    urlConnection.getInputStream());
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(content));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            urlConnection.disconnect();
+        }
+        return stringBuilder.toString();
+    }
+
+    private class ReadJSONFeedTask extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... urls) {
+            return readJSON(urls[0]);
+        }
+
+        protected void onPostExecute(String result) {
+            try {
+                JSONArray array = new JSONArray(result);
+                AppCompatTextView textView = (AppCompatTextView) findViewById(R.id.textView);
+                for (int i=0; i<array.length(); i++) {
+                    JSONObject obj = array.getJSONObject(i);
+                    String surveyDate = obj.getString("surveyDate");
+                    textView.setText(surveyDate);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
